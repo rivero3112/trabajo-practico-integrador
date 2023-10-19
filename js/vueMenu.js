daefd8ad4f38fb05ae67b4a6dc5e56db9d6851bd
NotFoundSN <julio.maldonado.sn@gmail.com>
2023-10-19 18:20:29 -0300

    salvaguardado temporal

---
+++ js/vueMenu.js
@@ -0,0 +1,17 @@
+/*import { createApp } from "vue";
+//import HeaderVue from "./vueComponent/header.js";
+
+createApp({
+	components: {
+		HeaderVue,
+	},
+	data() {
+		return {
+			groceryList: [
+				{ id: 0, text: "Vegetables" },
+				{ id: 1, text: "Cheese" },
+				{ id: 2, text: "Whatever else humans are supposed to eat" },
+			],
+		};
+	},
+}).mount("#app");*/
